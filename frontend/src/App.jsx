8c7f035333a2328ef07eb8d228c931481646719d
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-01-17 14:21:07 -0400

    added flowbite react to project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Test from "./components/Test";
 import Themetoggle from "./components/Themetoggle";
+import { Button } from "flowbite-react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Button>This is a flowbite button</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,16 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
