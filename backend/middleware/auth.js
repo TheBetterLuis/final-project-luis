c271c9844553558bda89580fa4115fe71e3af2e4
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-01-28 00:32:01 -0400

    .

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,23 +5,23 @@ function auth(roles = []) {
   return async function (req, res, next) {
     const token = req.headers["authorization"];
     if (!token) {
-      return res.status(403).json({ message: "token wasn't sent" });
+      return res.status(403).json({ message: "Token no fue enviado" });
     }
 
     try {
       const decoded = jwt.verify(token, process.env.SECRET_KEY);
-      console.log(decoded);
+      //      console.log(decoded);
       const userFind = await userModel.findById(decoded.id);
       if (!userFind) {
-        return res.status(404).json({ message: "user not found" });
+        return res.status(404).json({ message: "Usuario no encontrado" });
       }
       // console.log(userFind.role);
       if (!roles.includes(userFind.role)) {
-        return res.status(401).json({ message: "unauthorized access" });
+        return res.status(401).json({ message: "Acceso no autorizado" });
       }
       next();
     } catch (error) {
-      return res.status(401).json({ message: "invalid token" });
+      return res.status(401).json({ message: "Token invalido" });
     }
   };
 }
