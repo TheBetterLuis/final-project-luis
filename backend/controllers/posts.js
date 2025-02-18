6c0f3c2f0737d6eb8e4c45abf530a21182f972fd
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-02-17 23:57:33 -0400

    single view ticket page it's working, can delete/hide/unhide post also add comments, it also considers roles before it shows ticket

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -42,9 +42,27 @@ const getPostsByUserID = async (req, res) => {
 const getPostByTicketID = async (req, res) => {
   try {
     const { _id } = req.params;
-
+    /*
     const post = await postModel
       .find({ ticketID: _id })
+      .populate("ticketID")
+      .populate({
+        path: "userID",
+        model: "users",
+        select: "name lastName profilePicture",
+      })
+      .exec();
+      */
+    const post = await postModel
+      .find()
+      .populate({
+        path: "commentsID",
+        populate: {
+          path: "userID",
+          model: "users",
+          select: "name lastName profilePicture",
+        },
+      })
       .populate("ticketID")
       .populate({
         path: "userID",
--- frontend/src/pages/TicketViewPage.jsx
+++ frontend/src/pages/TicketViewPage.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
-import { Card } from "flowbite-react";
+import { Card, Button, Modal, TextInput } from "flowbite-react";
 import { FaHeart, FaCommentDots } from "react-icons/fa";
 
 import { formatDate } from "../../common/utils";
@@ -25,15 +25,15 @@ const TicketViewPage = () => {
           <div className="flex justify-center items-center h-screen w-full font-roboto overflow-y-scroll ">
             {/*COMPONENT GOES HERE*/}
             <div className="flex flex-col items-center">
-              <span class="flex items-stretch transition-all duration-200 rounded-md px-4 py-2 text-md">
+              <span className="flex items-stretch transition-all duration-200 rounded-md px-4 py-2 text-md">
                 Error al cargar ticket
               </span>
               <Link to={"/feed"}>
                 <button
                   type="button"
-                  class="group relative flex items-stretch justify-center p-0.5 text-center font-medium transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none border border-transparent focus:ring-4 focus:ring-cyan-300 enabled:hover:bg-cyan-800 dark:bg-cyan-600 dark:focus:ring-cyan-800 dark:enabled:hover:bg-cyan-700 bg-azul2 text-gray-100 rounded w-36 h-12 drop-shadow-lg hover:-translate-y-1 hover:scale-110 duration-300"
+                  className="group relative flex items-stretch justify-center p-0.5 text-center font-medium transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none border border-transparent focus:ring-4 focus:ring-cyan-300 enabled:hover:bg-cyan-800 dark:bg-cyan-600 dark:focus:ring-cyan-800 dark:enabled:hover:bg-cyan-700 bg-azul2 text-gray-100 rounded w-36 h-12 drop-shadow-lg hover:-translate-y-1 hover:scale-110 duration-300"
                 >
-                  <span class="flex items-stretch transition-all duration-200 rounded-md px-4 py-2 text-sm">
+                  <span className="flex items-stretch transition-all duration-200 rounded-md px-4 py-2 text-sm">
                     Volver al Feed
                   </span>
                 </button>
@@ -54,6 +54,9 @@ const TicketViewPage = () => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [modalData, setModalData] = useState(null);
+  const [newComment, setNewComment] = useState(null);
 
   useEffect(() => {
     const fetchInfo = async () => {
@@ -74,7 +77,7 @@ const TicketViewPage = () => {
         navigate("/login");
       }
     };
-    //679bb894e660b4fcf8674b9e
+
     const fetchPost = async () => {
       setLoading(true);
       try {
@@ -99,7 +102,83 @@ const TicketViewPage = () => {
     fetchPost();
   }, [navigate, ticketID]);
 
-  if (success) {
+  const handleDeletePost = async (postID) => {
+    // console.log(commentID);
+    const isConfirmed = window.confirm("Eliminar post?");
+
+    if (isConfirmed) {
+      try {
+        const response = await axios.delete(
+          `http://localhost:3001/api/posts/${postID}`
+        );
+      } catch (err) {
+        setError(err.response?.data?.message || "Error al eliminar post");
+      } finally {
+        window.location.reload();
+      }
+    }
+  };
+
+  const handleHidePost = async (postID) => {
+    let message = "";
+    let newStatus = "";
+
+    if (postData.status === "private") {
+      message = "Publicar Post?";
+      newStatus = "public";
+    }
+
+    if (postData.status === "public") {
+      message = "Ocultar Post?";
+      newStatus = "private";
+    }
+
+    const isConfirmed = window.confirm(message);
+
+    if (isConfirmed) {
+      try {
+        const response = await axios.patch(`http://localhost:3001/api/posts/`, {
+          id: postID,
+          status: newStatus,
+        });
+      } catch (err) {
+        setError(
+          err.response?.data?.message ||
+            "Error al al cambiar visibilidad del post"
+        );
+      } finally {
+        window.location.reload();
+      }
+    }
+  };
+
+  const handleNewComment = async (content) => {
+    const isConfirmed = window.confirm("Crear comentario?");
+
+    if (isConfirmed) {
+      try {
+        const response = await axios.post(
+          `http://localhost:3001/api/comments`,
+          {
+            postID: postData._id,
+            userID: userData.id,
+            content,
+          }
+        );
+      } catch (err) {
+        setError(err.response?.data?.message || "Error al crear comentario");
+      } finally {
+        setNewComment("");
+      }
+    }
+  };
+
+  if (
+    success &&
+    (userData.id === postData.userID._id ||
+      userData.role === "admin" ||
+      userData.role === "tech")
+  ) {
     returnPage = (
       <>
         <div className={` flex flex-col min-h-screen ${styles.background}`}>
@@ -127,13 +206,13 @@ const TicketViewPage = () => {
                         </span>
                       </div>
                     </div>
-                    <div class="p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-xs h-32 overflow-y-auto">
-                      <p class="text-black dark:text-gray-400">
+                    <div className="p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-xs h-32 overflow-y-auto">
+                      <p className="text-black dark:text-gray-400">
                         {postData.ticketID.description}
                       </p>
                     </div>
-                    <div class="p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-xs w-20 text-center">
-                      <p class="text-black dark:text-gray-400">
+                    <div className="p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-xs w-20 text-center">
+                      <p className="text-black dark:text-gray-400">
                         {formatDate(postData.ticketID.createdAt)}
                       </p>
                     </div>
@@ -150,22 +229,92 @@ const TicketViewPage = () => {
                     className="dark:text-white hover:text-red-500 dark:hover:text-red-500  cursor-pointer"
                   />
                   {userData.id === postData.userID._id && (
-                    <Button
-                      className="bg-red-500 hover:bg-gray-400"
-                      onClick={() => {
-                        handleDeletePost(postData._id);
-                      }}
-                    >
-                      Eliminar Post
-                    </Button>
+                    <>
+                      <Button
+                        className="bg-red-500 hover:bg-red-600"
+                        onClick={() => {
+                          handleDeletePost(postData._id);
+                        }}
+                      >
+                        <p className="text-xs">Eliminar Post</p>
+                      </Button>
+                      <Button
+                        className="bg-gray-500 hover:bg-gray-600"
+                        onClick={() => {
+                          handleHidePost(postData._id);
+                        }}
+                      >
+                        <p className="text-xs">
+                          {postData.status === "private"
+                            ? "Publicar Post"
+                            : "Ocultar Post"}
+                        </p>
+                      </Button>
+                    </>
                   )}
 
                   <FaCommentDots
                     size={26}
                     className="text-white hover:text-black dark:hover:text-gray-400 cursor-pointer"
+                    onClick={() => setOpenModal(true)}
                   />
                 </div>
               </Card>
+              <Modal
+                show={openModal}
+                size="md"
+                onClose={() => setOpenModal(false)}
+                popup
+              >
+                <Modal.Header />
+                <Modal.Body>
+                  <div className="text-center">
+                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                      {`Comentarios`}
+                    </h3>
+                    <>
+                      {postData.commentsID.map((comment, commentIndex) => (
+                        <div className="text-start" hidden={comment.hidden}>
+                          <h1 className="font-bold">{`${comment.userID.name} ${comment.userID.lastName}`}</h1>
+                          <div className="flex justify-between mb-2">
+                            <h2>{comment.content}</h2>
+                            {(userData.id === comment.userID._id ||
+                              userData.role === "admin") && (
+                              <h2
+                                className="font-bold text-red-600 hover:text-gray-500 cursor-pointer"
+                                onClick={() => {
+                                  // handleDeleteComment(
+                                  //  comment._id,
+                                  // commentIndex
+                                  //);
+                                }}
+                              >
+                                Eliminar
+                              </h2>
+                            )}
+                          </div>
+                        </div>
+                      ))}
+                    </>
+                    <div className="flex justify-between mt-12 px-4">
+                      <TextInput
+                        placeholder="Escribe aqui"
+                        value={newComment}
+                        onChange={(e) => setNewComment(e.target.value)}
+                      />
+
+                      <Button
+                        onClick={() => {
+                          handleNewComment(newComment);
+                        }}
+                        className="bg-azul2 drop-shadow-md"
+                      >
+                        Comentar
+                      </Button>
+                    </div>
+                  </div>
+                </Modal.Body>
+              </Modal>
             </div>
           </div>
           <PageFooter />
