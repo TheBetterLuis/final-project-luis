c0fe142ff911b700d309e5ba385729831441a4ca
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-02-20 15:11:22 -0400

    added stripe package and route to create payment session redirects to profile on cancel

---
+++ backend/controllers/payments.js
@@ -0,0 +1,25 @@
+const Stripe = require("stripe");
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const createSession = async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          product_data: {
+            name: "Plan Premium",
+            description: "Servicio Premium de atencion al cliente",
+          },
+          currency: "usd",
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "http://localhost:5173/#/create-invoice",
+    cancel_url: "http://localhost:5173/#/profile",
+  });
+  return res.json(session);
+};
+module.exports = { createSession };
