195b5f8849bb4c6748688f7d81c72a21d34b3084
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-02-25 02:06:19 -0400

    testing crud page in testPage

--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,121 +1,160 @@
+import CRUD from "../components/CRUD";
 import { NavBar } from "../components/NavBar";
 import PageFooter from "../components/Footer";
-import Post from "../components/Post";
 import CustomSidebar from "../components/CustomSidebar";
-import { jwtDecode } from "jwt-decode";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useState, useEffect } from "react";
-import { Button } from "flowbite-react";
-import Tarjeta from "../components/Tarjeta";
+
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const TestPage = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [userData, setUserData] = useState({});
-  const navigate = useNavigate();
-  const [postsData, setPostsData] = useState([]);
-  const [success, setSuccess] = useState(false);
-
   const styles = {
     background: "bg-gradient-to-tr from-azul4 via-[#52A2AB] to-azul1",
     background_feed:
       "bg-gradient-to-b from-[#EFFFFB] via-[#BFCCC8] to-[#8f9996]",
   };
 
-  const fetchInfo = async () => {
-    const token = localStorage.getItem("tokenSesion");
-    if (!token) {
-      navigate("/login");
-    }
-
-    const decoded = jwtDecode(token);
-    setUserData(decoded);
-
-    const expiry = decoded.exp;
-    const currentTime = Date.now() / 1000;
-
-    if (expiry < currentTime) {
-      console.log("token has expired");
-      localStorage.removeItem("tokenSesion");
-      navigate("/login");
-    }
-  };
-
-  const fetchPosts = async (page = 1) => {
-    try {
-      const response = await axios.post(
-        //67b413709dc0223f4e00024f
-        //67b4035e9dc0223f4e00004a
-        `http://localhost:3001/api/posts/paginate?page=${page}`
-      );
-      console.log("Post recibido:", response.data);
-      setPostsData((prevPosts) => [...prevPosts, ...response.data.posts]);
-      setTotalPages(response.data.totalPages);
-    } catch (err) {
-      console.error("Error al cargar el ticket", err);
-    }
-  };
+  const [userData, setUserData] = useState({});
+  const [currentPage, setCurrentPage] = useState("users");
+  const [pageData, setPageData] = useState([]);
+  const [error, setError] = useState();
+  const [currentUserPage, setCurrentUserPage] = useState(1);
+  const [currentTechPage, setCurrentTechPage] = useState(1);
+  const navigate = useNavigate();
 
   useEffect(() => {
+    const fetchInfo = async () => {
+      const token = localStorage.getItem("tokenSesion");
+      if (!token) {
+        navigate("/login");
+      }
+
+      const decoded = jwtDecode(token);
+      setUserData(decoded);
+      const userRole = decoded.role;
+
+      if (userRole !== "admin") {
+        navigate("/login");
+      }
+
+      const expiry = decoded.exp;
+      const currentTime = Date.now() / 1000;
+
+      if (expiry < currentTime) {
+        console.log("token has expired");
+        localStorage.removeItem("tokenSesion");
+        navigate("/login");
+      }
+
+      setLoading(false);
+    };
     fetchInfo();
-    fetchPosts(1);
   }, [navigate]);
 
-  const loadNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-    fetchPosts(currentPage + 1);
-    console.log("loaded more");
+  useEffect(() => {
+    if (userData && userData.id) {
+      fetchUsers(currentPage === "users" ? "users" : "tech", 1);
+    }
+  }, [userData]);
+
+  const fetchUsers = async (usersToFetch, page = 1) => {
+    let url = "";
+
+    if (usersToFetch === "users") {
+      url = `http://localhost:3001/api/users/regular/paginate?page=${page}`;
+    }
+
+    if (usersToFetch === "tech") {
+      url = `http://localhost:3001/api/users/tech/paginate?page=${page}`;
+    }
+
+    try {
+      const response = await axios.get(url);
+      setPageData(response.data.users);
+      console.log(response.data);
+    } catch (err) {
+      console.error("Error al cargar los usuarios", err);
+      setError(
+        err.response?.data?.message ||
+          "Ha ocurrido un error durante la carga de posts"
+      );
+    } finally {
+      setCurrentPage(usersToFetch);
+    }
   };
-
-  let pageContent = (
-    <>
-      {postsData === null && (
-        <>
-          <Button className="bg-gray-400 drop-shadow-md">
-            <h1 className="text-2xl">No hay informacion disponible</h1>
-          </Button>
-        </>
-      )}
-    </>
-  );
-
-  if (success) {
-    pageContent = (
-      <>
-        {postsData !== null && userData !== null && (
-          <>
-            {postsData.map((post, index) => (
-              <Post data={post} userData={userData} />
-            ))}
-          </>
-        )}
-      </>
-    );
-  }
 
   return (
     <>
-      <div className={`${styles.background_feed}`}>
+      <div className={`${styles.background}`}>
         <NavBar />
         <div
           id="wrapper"
           className="pt-24 pb-28 min-h-screen flex flex-col items-center justify-center"
         >
-          {/*Page Component goes here*/}
-          <div
-            className={`${styles.background} fixed top-0 left-0 w-screen h-1/4 z-1 `}
-          ></div>
-          <Tarjeta className="relative " userData={userData} />
-          <Button className=" md:block md:top-40 md:left-28 lg:left-32 xl:left-72  2xl:left-96 bg-azul4 absolute top-40 left-60 top-20 left-20 sm:left-40 z-10">
-            Plan
-          </Button>
-          <Button className="md:block md:top-40 md:right-16 lg:right-32 xl:right-52 2xl:right-96 bg-azul4 absolute top-40 right-[300px] top-20 right-20 sm:right-40 z-10">
-            Configuracion
-          </Button>
-          {pageContent}
-
-          {/*Page Component goes here*/}
+          <div className="flex h-screen w-full  mt-16 mb-24">
+            <div className="flex flex-col w-full h-screen justify-evenly items-center">
+              <div className="flex flex-row justify-center items-center space-x-4 sm:space-x-80">
+                <button
+                  className="bg-azul4 text-white px-10 py-3 sm:px-20 sm:py-6 rounded-lg text-xl hover:-translate-y-1 hover:scale-110  duration-300  drop-shadow-lg my-6"
+                  onClick={() => {
+                    setCurrentUserPage(1);
+                    fetchUsers("users", 1);
+                  }}
+                >
+                  Usuarios
+                </button>
+                <button
+                  className="bg-azul4 text-white px-10 py-3 sm:px-20 sm:py-6 rounded-lg text-xl hover:-translate-y-1 hover:scale-110  duration-300  drop-shadow-lg my-6"
+                  onClick={() => {
+                    setCurrentTechPage(1);
+                    fetchUsers("tech", 1);
+                  }}
+                >
+                  Tecnicos
+                </button>
+              </div>
+              <CRUD
+                view={currentPage}
+                data={pageData}
+                fetchUsers={fetchUsers}
+              />
+              <div className="flex flex-row justify-center items-center space-x-4">
+                <button
+                  className="bg-gray-200 text-black px-4 py-2 rounded-lg"
+                  onClick={() => {
+                    if (currentPage === "users") {
+                      setCurrentUserPage((prev) => Math.max(prev - 1, 1));
+                      fetchUsers("users", Math.max(currentUserPage - 1, 1));
+                    } else if (currentPage === "tech") {
+                      setCurrentTechPage((prev) => Math.max(prev - 1, 1));
+                      fetchUsers("tech", Math.max(currentTechPage - 1, 1));
+                    }
+                  }}
+                  disabled={
+                    (currentPage === "users" && currentUserPage === 1) ||
+                    (currentPage === "tech" && currentTechPage === 1)
+                  }
+                >
+                  Anterior
+                </button>
+                <button
+                  className="bg-gray-200 text-black px-4 py-2 rounded-lg"
+                  onClick={() => {
+                    if (currentPage === "users") {
+                      setCurrentUserPage((prev) => prev + 1);
+                      fetchUsers("users", currentUserPage + 1);
+                    } else if (currentPage === "tech") {
+                      setCurrentTechPage((prev) => prev + 1);
+                      fetchUsers("tech", currentTechPage + 1);
+                    }
+                  }}
+                >
+                  Siguiente
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
         <PageFooter />
       </div>
